04040b0aa1a45a8c0ea3294b8c27b749eba19d5d
m-31 <michael@meyling.com>
2015-11-03 20:19:09 +0100

    more ascii art

--- spec/art_spec.rb
+++ spec/art_spec.rb
@@ -5,13 +5,44 @@ describe Vamp::Art do
     expect(subject::VAMPIRE).not_to be nil
     puts "\n#{subject::VAMPIRE}"
   end
-
-  it "has a homoglobin" do
-    expect(subject::HOBOGLOBIN_COLOR).not_to be nil
-    puts "\n#{subject::HOBOGLOBIN_COLOR}"
+  it "has a colorful hobgoblin" do
+    expect(subject::HOBGOBLIN_COLOR).not_to be nil
+    puts "\n#{subject::HOBGOBLIN_COLOR}"
   end
-  it "has a scull" do
-    expect(subject::SCULL_COLOR).not_to be nil
-    puts "\n#{subject::SCULL_COLOR}"
+  it "has a hobgoblin" do
+    expect(subject::HOBGOBLIN).not_to be nil
+    puts "\n#{subject::HOBGOBLIN}"
+  end
+  it "has a colorful shark" do
+    expect(subject::SHARK_COLOR).not_to be nil
+    puts "\n#{subject::SHARK_COLOR}"
+  end
+  it "has a shark" do
+    expect(subject::SHARK).not_to be nil
+    puts "\n#{subject::SHARK}"
+  end
+  it "has a colorful skull" do
+    expect(subject::SKULL_COLOR).not_to be nil
+    puts "\n#{subject::SKULL_COLOR}"
+  end
+  it "has a skull" do
+    expect(subject::SKULL).not_to be nil
+    puts "\n#{subject::SKULL}"
+  end
+  it "has a colorful lizard" do
+    expect(subject::LIZARD_COLOR).not_to be nil
+    puts "\n#{subject::LIZARD_COLOR}"
+  end
+  it "has a lizard" do
+    expect(subject::LIZARD).not_to be nil
+    puts "\n#{subject::LIZARD}"
+  end
+  it "has a colorful dragon" do
+    expect(subject::DRAGON_COLOR).not_to be nil
+    puts "\n#{subject::DRAGON_COLOR}"
+  end
+  it "has a dragon" do
+    expect(subject::DRAGON).not_to be nil
+    puts "\n#{subject::DRAGON}"
   end
 end
