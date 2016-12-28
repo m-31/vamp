10755a608e6189d6f7ea35a880778e6d5d195742
m-31 <michael@meyling.com>
2016-12-28 15:29:59 +0100

    make vamp 1.8.7 compatible

--- lib/vamp.rb
+++ lib/vamp.rb
@@ -1,4 +1,4 @@
-require 'rubygem'        if RUBY_VERSION =~ /^1\.8\./
+require 'rubygems'       if RUBY_VERSION =~ /^1\.8\./
 require 'extensions/all' if RUBY_VERSION =~ /^1\.8\./
 require "vamp/animator"
 require "vamp/art"
